0cf52fc22b5175111985df48e71376cb3a6c36b8
Shimina Larisa <shimina@a-3.ru>
2025-07-14 17:35:53 +0300

    Добавит тип MockServerData

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -3,3 +3,4 @@ export { CitiesType } from './cities-type.enum.js';
 export { Comment } from './comment.type.js';
 export { Offer } from './offer.type.js';
 export { User } from './user.type.js';
+export { MockServerData } from './mock-server-data.type.js';
---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,17 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  postDates: string[];
+  cities: string[];
+  previewImages: string[];
+  images: string[];
+  isPremium: boolean[];
+  isFavorite: boolean[];
+  types: string[];
+  goods: string[];
+  users: string[];
+  isPro: boolean[];
+  emails: string[];
+  avatars: string[];
+  locations: {latitude: number; longitude: number}[];
+};
