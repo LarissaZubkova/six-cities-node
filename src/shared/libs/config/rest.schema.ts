f9b104dfd231e191cd0478b27c67be126d41ab55
Shimina Larisa <shimina@a-3.ru>
2025-12-23 14:14:59 +0300

    Расширит тип RestSchema

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -2,6 +2,8 @@ import convict from 'convict';
 
 export type RestSchema = {
   PORT: number;
+  SALT: string;
+  DB_HOST: string;
 }
 
 export const configRestSchema = convict<RestSchema>({
@@ -10,5 +12,17 @@ export const configRestSchema = convict<RestSchema>({
     format: 'port',
     env: 'PORT',
     default: 4000
-  }
+  },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null
+  },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: String,
+    env: 'DB_HOST',
+    default: '127.0.0.1'
+  },
 });
