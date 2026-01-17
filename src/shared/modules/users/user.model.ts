36ff5cd0f9582c15fe5de0129e716475d6cbc6d1
Shimina Larisa <shimina@a-3.ru>
2026-01-17 22:00:24 +0300

    Задействует встроенную валидацию

--- src/shared/modules/users/user.model.ts
+++ src/shared/modules/users/user.model.ts
@@ -7,13 +7,20 @@ export interface UserDocument extends User, Document {
 }
 
 const userSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    minlength: [1, 'Min length for name is 1'],
+    maxlength: [15, 'Min length for name is 15'],
+    require: true,
+  },
   email: {
     type: String,
     unique: true,
+    require: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
   },
   userType: String,
   avatarPath: String,
-});
+}, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
