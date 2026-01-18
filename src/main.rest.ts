85fc68a546f39da922b9f3bba11c0d710711e385
Shimina Larisa <shimina@a-3.ru>
2026-01-18 22:11:32 +0300

    Добавит внедрение зависимостей в DefaultUserService

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -2,9 +2,10 @@ import 'reflect-metadata';
 import { Container } from 'inversify';
 import { createRestApplicationContainer, RestApplication } from './rest/index.js';
 import { Component } from './shared/types/index.js';
+import { createUserContainer } from './shared/modules/users/index.js';
 
 async function bootstrap() {
-  const appContainer = Container.merge(createRestApplicationContainer());
+  const appContainer = Container.merge(createRestApplicationContainer(), createUserContainer());
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   application.init();
--- src/shared/modules/users/default-user.service.ts
+++ src/shared/modules/users/default-user.service.ts
@@ -1,12 +1,23 @@
 import { DocumentType } from '@typegoose/typegoose';
-import { UserEntity, UserModel } from './user.entity.js';
 import { CreateUserDto, UserService } from './index.js';
+import { inject, injectable } from 'inversify';
+import { Component } from '../../types/index.js';
+import { Logger } from '../../libs/logger/index.js';
+import { UserEntity, UserModel } from './user.entity.js';
 
+@injectable()
 export class DefaultUserService implements UserService {
+  constructor(
+    @inject(Component.Logger) private readonly logger: Logger,
+  ){}
+
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
-    return UserModel.create(user);
+    const result = UserModel.create(user);
+    this.logger.info(`New user created: ${user.email}`);
+
+    return result;
   }
 }
