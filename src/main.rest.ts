0f9791916767b67c6143c95f15aafd8f7ec7fc4d
Shimina Larisa <shimina@a-3.ru>
2026-01-18 21:57:26 +0300

    Разобьет общий контейнер

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,19 +1,12 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { RestApplication } from './rest/index.js';
-import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
-import { Logger, PinoLogger } from './shared/libs/logger/index.js';
+import { createRestApplicationContainer, RestApplication } from './rest/index.js';
 import { Component } from './shared/types/index.js';
-import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inRequestScope();
-  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
+  const appContainer = Container.merge(createRestApplicationContainer());
 
-  const application = container.get<RestApplication>(Component.RestApplication);
+  const application = appContainer.get<RestApplication>(Component.RestApplication);
   application.init();
 }
 
