9d6d706246899ba4e0d85dcd7435a0ff3cc45473
Shimina Larisa <shimina@a-3.ru>
2026-01-18 22:18:30 +0300

    Добавит внедрение модели в зависимости

--- src/shared/modules/users/default-user.service.ts
+++ src/shared/modules/users/default-user.service.ts
@@ -1,4 +1,4 @@
-import { DocumentType } from '@typegoose/typegoose';
+import { DocumentType, types } from '@typegoose/typegoose';
 import { CreateUserDto, UserService } from './index.js';
 import { inject, injectable } from 'inversify';
 import { Component } from '../../types/index.js';
@@ -9,13 +9,14 @@ import { UserEntity, UserModel } from './user.entity.js';
 export class DefaultUserService implements UserService {
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.UserModel) private readonly userModel: types.ModelType<UserEntity>,
   ){}
 
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
-    const result = UserModel.create(user);
+    const result = await this.userModel.create(user);
     this.logger.info(`New user created: ${user.email}`);
 
     return result;
