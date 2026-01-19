38859347d0fbc69e5b2f1c95e2733074ba24b262
Shimina Larisa <shimina@a-3.ru>
2026-01-19 22:22:34 +0300

    Обновит команду импорт

---
+++ src/cli/commands/command.constants.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_DB_PORT = '21017';
+export const DEFAULT_USER_PASSWORD = '1234';
--- src/shared/modules/offers/offer-service.interface.ts
+++ src/shared/modules/offers/offer-service.interface.ts
@@ -2,6 +2,6 @@ import { DocumentType } from '@typegoose/typegoose';
 import { CreateOfferDto, OfferEntity } from './index.js';
 
 export interface OfferService {
-  create(dto: CreateOfferDto, salt: string): Promise<DocumentType<OfferEntity>>;
+  create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(id: string): Promise<DocumentType<OfferEntity> | null>;
 }
